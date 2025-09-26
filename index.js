21a29153029ce4db58b24d0691533c07bbbb50bd
yadhukrishna1322 <yadhukrishnanp1322@gmail.com>
2025-09-26 12:22:13 +0530

    ci/cd

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 3000;
 
 // Define a route
 app.get("/", (req, res) => {
-  res.send("Congratulations HHLD Folks - we are in ECS Demo!");
+  res.send(
+    "Congratulations HHLD Folks - we are in ECS Demo version ! ya mone cicd!"
+  );
 });
 
 app.get("/health", (req, res) => {
